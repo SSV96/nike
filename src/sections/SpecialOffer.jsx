800ecd6dee530f2e7e1994a79cc41e1c31db81ce
SSV96 <sathyaswaroopvandavasi@gmail.com>
2024-11-26 21:19:09 +0530

    complted Nike Website

--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,5 +1,43 @@
+import Button from "../components/Button";
+import { offer } from "../assets/images";
+import { arrowRight } from "../assets/icons";
 const SpecialOffer = () => {
-  return <div>SpecialOffer</div>;
+  return (
+    <section className="flex justify-wrap items-center max-xl:flex-col-reverse gap-10">
+      <div className="flex-1">
+        {" "}
+        <img src={offer} width={773} height={687} className="object-contain" />
+      </div>
+      <div className="flex flex-1 flex-col">
+        <p className="text-xl font-montserrat text-coral-red">
+          Our Summer Collection
+        </p>
+        <h2 className=" font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
+          <span className="text-coral-red">Special </span> Offer
+        </h2>
+        <p className=" mt-4 lg:max-w-lg info-text ">
+          Ensuring premium comfort and style, our meticulously crafted footwear
+          is designed to last. We pride ourselves on our commitment to quality
+          and our dedication to providing our customers with the best possible
+          experience.
+        </p>
+        <p className="mt-6 lg:max-w-lg info-text">
+          Our dedication to detail and excellence has led us to create a range
+          of shoes that cater to a variety of foot shapes and sizes.
+        </p>
+        <div className="mt-11 flex flex-wrap gap-4">
+          {" "}
+          <Button label="Shop Now" iconURL={arrowRight} />{" "}
+          <Button
+            label="Learn More"
+            backgroundColor="bg-white"
+            borderColor="border-slate-gray"
+            textColor="text-slate-gray"
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default SpecialOffer;
